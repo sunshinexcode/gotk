122790d42c9d8cfbf0ac2f12f29264e358860f06
sunshinexcode <24xinhui@163.com>
2025-03-31 16:12:43 +0800

    chore(): optimize comment

--- vaes/gcm_aes_test.go
+++ vaes/gcm_aes_test.go
@@ -9,30 +9,30 @@ import (
 	"github.com/sunshinexcode/gotk/vtest"
 )
 
-func TestEncryptGCM(t *testing.T) {
-	dst, err := vaes.EncryptGCM([]byte("test"), vmd5.GetByte("test"), vmd5.GetByte("test")[:12])
+func TestDecryptGcm(t *testing.T) {
+	_, err := vaes.DecryptGcm([]byte("test"), vmd5.GetByte("test"), vmd5.GetByte("test")[:12])
 
-	vtest.Nil(t, err)
-	vtest.Equal(t, "IBXU2jtsmYZBNoxnjINuTHreR0k=", vbase64.EncodeToStr(dst))
+	vtest.NotNil(t, err)
+	vtest.Equal(t, "cipher: message authentication failed", err.Error())
 
-	src, err := vaes.DecryptGCM(dst, vmd5.GetByte("test"), vmd5.GetByte("test")[:12])
-
-	vtest.Nil(t, err)
-	vtest.Equal(t, "test", string(src))
-
-	_, err = vaes.EncryptGCM([]byte("test"), vmd5.GetByte("test")[:1], vmd5.GetByte("test")[:12])
+	_, err = vaes.DecryptGcm([]byte("test"), vmd5.GetByte("test")[:1], vmd5.GetByte("test")[:12])
 
 	vtest.NotNil(t, err)
 	vtest.Equal(t, "crypto/aes: invalid key size 1", err.Error())
 }
 
-func TestDecryptGCM(t *testing.T) {
-	_, err := vaes.DecryptGCM([]byte("test"), vmd5.GetByte("test"), vmd5.GetByte("test")[:12])
+func TestEncryptGcm(t *testing.T) {
+	dst, err := vaes.EncryptGcm([]byte("test"), vmd5.GetByte("test"), vmd5.GetByte("test")[:12])
 
-	vtest.NotNil(t, err)
-	vtest.Equal(t, "cipher: message authentication failed", err.Error())
+	vtest.Nil(t, err)
+	vtest.Equal(t, "IBXU2jtsmYZBNoxnjINuTHreR0k=", vbase64.EncodeToStr(dst))
 
-	_, err = vaes.DecryptGCM([]byte("test"), vmd5.GetByte("test")[:1], vmd5.GetByte("test")[:12])
+	src, err := vaes.DecryptGcm(dst, vmd5.GetByte("test"), vmd5.GetByte("test")[:12])
+
+	vtest.Nil(t, err)
+	vtest.Equal(t, "test", string(src))
+
+	_, err = vaes.EncryptGcm([]byte("test"), vmd5.GetByte("test")[:1], vmd5.GetByte("test")[:12])
 
 	vtest.NotNil(t, err)
 	vtest.Equal(t, "crypto/aes: invalid key size 1", err.Error())
