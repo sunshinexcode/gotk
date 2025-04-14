11808161c70773a4ef52a80bc680119821b004fd
sunshinexcode <24xinhui@163.com>
2025-04-14 14:42:52 +0800

    chore(): optimize vcache

--- vcache/cache_test.go
+++ vcache/cache_test.go
@@ -13,6 +13,50 @@ func TestCheckDataEmpty(t *testing.T) {
 	vtest.Equal(t, false, vcache.CheckDataEmpty("test"))
 }
 
+// TestContains tests the Contains function
+func TestContains(t *testing.T) {
+	// Test non-existent key
+	exists, err := vcache.Contains(context.TODO(), "key_contains")
+
+	vtest.Nil(t, err)
+	vtest.Equal(t, false, exists)
+
+	// Test existing key
+	err = vcache.Set(context.TODO(), "key_contains", "val_contains", 0)
+
+	vtest.Nil(t, err)
+
+	exists, err = vcache.Contains(context.TODO(), "key_contains")
+
+	vtest.Nil(t, err)
+	vtest.Equal(t, true, exists)
+
+	// Test with different key types
+	err = vcache.Set(context.TODO(), 123, "val_contains_int", 0)
+
+	vtest.Nil(t, err)
+
+	exists, err = vcache.Contains(context.TODO(), 123)
+
+	vtest.Nil(t, err)
+	vtest.Equal(t, true, exists)
+
+	// Test with struct key
+	type testKey struct {
+		ID   int
+		Name string
+	}
+	key := testKey{ID: 1, Name: "test"}
+	err = vcache.Set(context.TODO(), key, "val_contains_struct", 0)
+
+	vtest.Nil(t, err)
+
+	exists, err = vcache.Contains(context.TODO(), key)
+
+	vtest.Nil(t, err)
+	vtest.Equal(t, true, exists)
+}
+
 func TestGet(t *testing.T) {
 	data, err := vcache.Get(context.TODO(), "key_get")
 
@@ -30,6 +74,79 @@ func TestGetOrSet(t *testing.T) {
 
 	vtest.Nil(t, err)
 	vtest.Equal(t, "val_get_or_set", data.String())
+}
+
+func TestRemove(t *testing.T) {
+	// Test removing non-existent key
+	val, err := vcache.Remove(context.TODO(), "key_remove")
+
+	vtest.Nil(t, err)
+	vtest.Equal(t, "", val.String())
+
+	// Test removing existing key
+	err = vcache.Set(context.TODO(), "key_remove", "val_remove", 0)
+
+	vtest.Nil(t, err)
+
+	exists, err := vcache.Contains(context.TODO(), "key_remove")
+
+	vtest.Nil(t, err)
+	vtest.Equal(t, true, exists)
+
+	val, err = vcache.Remove(context.TODO(), "key_remove")
+
+	vtest.Nil(t, err)
+	vtest.Equal(t, "val_remove", val.String())
+
+	exists, err = vcache.Contains(context.TODO(), "key_remove")
+
+	vtest.Nil(t, err)
+	vtest.Equal(t, false, exists)
+
+	// Test removing with different key types
+	err = vcache.Set(context.TODO(), 123, "val_remove_int", 0)
+
+	vtest.Nil(t, err)
+
+	exists, err = vcache.Contains(context.TODO(), 123)
+
+	vtest.Nil(t, err)
+	vtest.Equal(t, true, exists)
+
+	val, err = vcache.Remove(context.TODO(), 123)
+
+	vtest.Nil(t, err)
+	vtest.Equal(t, "val_remove_int", val.String())
+
+	exists, err = vcache.Contains(context.TODO(), 123)
+
+	vtest.Nil(t, err)
+	vtest.Equal(t, false, exists)
+
+	// Test removing struct key
+	type testKey struct {
+		ID   int
+		Name string
+	}
+	key := testKey{ID: 1, Name: "test"}
+	err = vcache.Set(context.TODO(), key, "val_remove_struct", 0)
+
+	vtest.Nil(t, err)
+
+	exists, err = vcache.Contains(context.TODO(), key)
+
+	vtest.Nil(t, err)
+	vtest.Equal(t, true, exists)
+
+	val, err = vcache.Remove(context.TODO(), key)
+
+	vtest.Nil(t, err)
+	vtest.Equal(t, "val_remove_struct", val.String())
+
+	exists, err = vcache.Contains(context.TODO(), key)
+
+	vtest.Nil(t, err)
+	vtest.Equal(t, false, exists)
 }
 
 func TestSet(t *testing.T) {
